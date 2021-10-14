f53fb62829e2ba7c56ab40e4ad2f08389212d496
robbiecoderr <80084937+robbiecoderr@users.noreply.github.com>
2021-10-14 19:58:38 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("NOITCNUF", 700,150)
+fill("lightblue")
+text("HINT:it helps us execute a block of code", 700,170)
+
+
+fill("white")
+textSize(15)
+text("EBASATAD", 100,350)
+fill("lightblue")
+text("Hint:stores all the data of the game", 100,370)
+
+}
